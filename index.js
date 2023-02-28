e7e24e9b110536b6597061ff9e2f232fd0ebd179
Jose <jose@MacBook-Pro-de-Jose-2.local>
2023-02-28 12:38:16 +0000

    finished home

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 const express = require("express");
 
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require("path");
 
 app.use(express.static("public"));
 app.use(express.static("files"));
+app.use(connectLiveReload());
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "/pages/home.html"));
 });
 
 app.use("/static", express.static("static"));
+app.use("/components", express.static("components"));
 
 app.get("/about_us", function (req, res) {
-  res.sendFile(path.join(__dirname, "/pages/about_us/about_us.html"));
+  res.sendFile(path.join(__dirname, "/pages/about_us.html"));
 });
 
 app.get("/documentation", function (req, res) {
-  res.sendFile(path.join(__dirname, "/pages/documentation/documentation.html"));
+  res.sendFile(path.join(__dirname, "/pages/documentation.html"));
 });
 
 app.get("/news", function (req, res) {
-  res.sendFile(path.join(__dirname, "/pages/news/news.html"));
+  res.sendFile(path.join(__dirname, "/pages/news.html"));
+});
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
 });
 
 app.listen(port);
