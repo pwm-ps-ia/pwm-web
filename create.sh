a070bf347e185e9a01812f8dd372ffe627293fde
Joaquín Villamonte Pereira <jjvvpp_2001@protonmail.com>
2023-03-04 19:10:05 +0000

    Make create.sh append SCSS include

--- create.sh
+++ create.sh
@@ -5,6 +5,7 @@ function create_component(){
     mkdir -p "static/scss/$1" "$1"
 
     touch "$1/$2.html" "static/scss/$1/$2.scss"
+    echo "@import \"./$2.scss\";" >> "static/scss/$1/index.scss"
 }
 
 case $command in
